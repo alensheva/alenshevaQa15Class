69390f19cc3fc1fda6f2c295390665995695e360
alensheva <ddffds>
2018-09-20 20:41:01 +0300

    Lesson open browsers in Selenium

---
+++ firstSeleniumProject/src/test/java/OpenChromeTest.java
@@ -0,0 +1,24 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class OpenChromeTest {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+        driver = new ChromeDriver();
+
+    }
+
+    @Test
+    public void testOpenBrowser(){
+        driver.navigate().to("https://btl.gov.il/Pages/default.aspx");
+    }
+    @AfterMethod
+    public void tearDown(){
+      driver.quit();
+    }
+}
