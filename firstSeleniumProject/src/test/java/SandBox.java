99a03f7bd15a1d846d3b68c5bfb3726411e0e060
alensheva <ddffds>
2018-10-04 19:48:56 +0300

    addirions

--- firstSeleniumProject/src/test/java/SandBox.java
+++ firstSeleniumProject/src/test/java/SandBox.java
@@ -1,3 +1,10 @@
+// ZAMETKI
+//wd.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);vremya kotoroe daet selenium na zagruzky saita
+//   Thread.sleep(2000); vremya kotoroe sait ostaetsya otkritim do avtozakritiya
+
+
 public class SandBox {
+
+
 
 }
